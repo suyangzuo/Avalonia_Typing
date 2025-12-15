423eb78d9f8289f65bf1be6dcc7491676c6b4a94
suyangzuo <suyangzuo@163.com>
2025-12-15 17:33:30 +0800

    初始提交

---
+++ Avalonia_Typing/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia_Typing.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Avalonia_Typing/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia_Typing.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
