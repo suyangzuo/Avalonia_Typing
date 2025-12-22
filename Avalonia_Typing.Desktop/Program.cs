36a4ca8e69c602209f9fbb871d03acacc99fe2a2
suyangzuo <suyangzuo@163.com>
2025-12-22 12:38:25 +0800

    解决了Linux平台下Skia本地库版本太低导致的问题

--- Avalonia_Typing.Desktop/Program.cs
+++ Avalonia_Typing.Desktop/Program.cs
@@ -9,8 +9,13 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // 在 Linux 上，确保优先从应用程序目录加载本地库
+        Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", AppContext.BaseDirectory);
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
